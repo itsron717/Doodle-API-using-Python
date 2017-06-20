ae51b9842d7982f6210672f42fbdd2e21d9cf475
Rounak Vyas <itsron143@gmail.com>
2017-06-20 23:16:51 +0530

    Update Doodle.py

--- Doodle.py
+++ Doodle.py
@@ -6,58 +6,77 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import WebDriverException
 
+
 def doodle():
-    name = "Rounak Vyas"  # input("Name: \n")
-    email = "rounakv1@gmail.com"  # input("Email: \n")
-    eventTitle = "sdfsd"  # input("Event Title: \n")
-    description = "waedawsd"  # input("Description: \n")
-    location = "asdas"  # input("Location: \n")
-    fromDate = "20170617"  # input("Date(Day 1): \n")
-    _time = "1200-1300"  # input("Time Interval: \n")
-    _time2 = "1300-1500"  # input("Time Interval(2): \n")
-    fromDate2 = "20170618"  # input("Date(Day 2): \n")
-    _dtime = "1300-1900"  # input("Time Interval: \n")
-    _dtime2 = "1400-1900"  # input("Time Interval(2, Optional): \n")
-    fromDate3 = "20170619"  # input("Date(Day 3): \n")
-    _ddtime = "1300-1900"  # input("Time Interval: \n")
-    _ddtime2 = "1400-1800"  # input("Time Interval(2, Optional): \n")
-    
-    doodUrl = "http://doodle.com/create?type=date&locale=en&title=" + eventTitle + "&name=" + name + "&description=" + description + "&" + fromDate + "=" + _time + "||" + _time2 + "&" + fromDate2 + "=" + _dtime + "||" + _dtime2 + "&" + fromDate3 + "=" + _ddtime + "||" + _ddtime2 + "&location=" + location
+    name = input("Name: \n")
+    email = input("Email: \n")
+    eventTitle = input("Event Title: \n")
+    description = input("Description: \n")
+    location = input("Location: \n")
+    fromDate = input("Date(Day 1):\n(Format: YY-MM-DD)\n(Example: 2017-01-01) \n")
+    fromDate = fromDate[0:4] + fromDate[5:7] + fromDate[8:10]
+    _time = input("Time Interval:\n(Example- 07:00-14:00) \n")
+    _time = _time[0:2] + _time[3:8] + _time[9:11]
+    _time2 = input("Time Interval(2): \n")
+    _time2 = _time2[0:2] + _time2[3:8] + _time2[9:11]
+    fromDate2 = input("Date(Day 2): \n")
+    fromDate2 = fromDate2[0:4] + fromDate2[5:7] + fromDate2[8:10]
+    _dtime = input("Time Interval: \n")
+    _dtime = _dtime[0:2] + _dtime[3:8] + _dtime[9:11]
+    _dtime2 = input("Time Interval(2, Optional): \n")
+    _dtime2 = _dtime2[0:2] + _dtime2[3:8] + _dtime2[9:11]
+    fromDate3 = input("Date(Day 3): \n")
+    fromDate3 = fromDate3[0:4] + fromDate3[5:7] + fromDate3[8:10]
+    _ddtime = input("Time Interval: \n")
+    _ddtime = _ddtime[0:2] + _ddtime[3:8] + _ddtime[9:11]
+    _ddtime2 = input("Time Interval(2, Optional): \n")
+    _ddtime2 = _ddtime2[0:2] + _ddtime2[3:8] + _ddtime2[9:11]
+
+    doodUrl = "http://doodle.com/create?type=date&locale=en&title=" + eventTitle + "&name=" + name + "&description=" + description + "&" + fromDate + "=" + \
+        _time + "||" + _time2 + "&" + fromDate2 + "=" + _dtime + "||" + _dtime2 + \
+        "&" + fromDate3 + "=" + _ddtime + "||" + _ddtime2 + "&location=" + location
 
     driver = webdriver.Chrome()
     driver.get(doodUrl)
     try:
-        initemail = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("initiatorEmail"))
+        initemail = WebDriverWait(driver, 10).until(
+            lambda driver: driver.find_element_by_id("initiatorEmail"))
         driver.find_element_by_id("initiatorEmail").send_keys(email)
         next1 = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("next1"))
         if driver.find_element_by_id("next1").is_enabled() and driver.find_element_by_id("next1").is_displayed():
             pass
         else:
-            next1 = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("next1"))
+            next1 = WebDriverWait(driver, 10).until(
+                lambda driver: driver.find_element_by_id("next1"))
         next1.click()
         next2a = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("next2a"))
         if driver.find_element_by_id("next2a").is_enabled() and driver.find_element_by_id("next2a").is_displayed():
             pass
         else:
-            next2a = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("next2a"))
+            next2a = WebDriverWait(driver, 10).until(
+                lambda driver: driver.find_element_by_id("next2a"))
         next2a.click()
         next2b = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("next2b"))
         if driver.find_element_by_id("next2b").is_enabled() and driver.find_element_by_id("next2b").is_displayed():
             pass
         else:
-            next2b = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("next2b"))
+            next2b = WebDriverWait(driver, 10).until(
+                lambda driver: driver.find_element_by_id("next2b"))
         next2b.click()
         next3s = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("next3s"))
         if driver.find_element_by_id("next3s").is_enabled() and driver.find_element_by_id("next3s").is_displayed():
             pass
         else:
-            next3s = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("next3s"))
+            next3s = WebDriverWait(driver, 10).until(
+                lambda driver: driver.find_element_by_id("next3s"))
         next3s.click()
-        finish4a = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("finish4a"))
+        finish4a = WebDriverWait(driver, 10).until(
+            lambda driver: driver.find_element_by_id("finish4a"))
         if driver.find_element_by_id("finish4a").is_enabled() and driver.find_element_by_id("finish4a").is_displayed():
             pass
         else:
-            finish4a = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("finish4a"))
+            finish4a = WebDriverWait(driver, 10).until(
+                lambda driver: driver.find_element_by_id("finish4a"))
         finish4a.click()
         try:
             element = WebDriverWait(driver, 10).until(
@@ -85,4 +104,6 @@ def doodle():
         print("Element is not clickable.")
         print("Please try again.")
     driver.quit()
+    time.sleep(10)
+
 doodle()
